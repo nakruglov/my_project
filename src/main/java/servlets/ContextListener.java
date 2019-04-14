5de06e2ee05dd9ed083e2c01000fcbda1cec6e62
nakruglov <kruglovnick@gmail.com>
2019-04-15 00:08:44 +0300

    init 2

---
+++ src/main/java/servlets/ContextListener.java
@@ -0,0 +1,49 @@
+package servlets;
+
+import org.apache.commons.dbcp2.ConnectionFactory;
+import org.apache.commons.dbcp2.DriverManagerConnectionFactory;
+import org.apache.commons.dbcp2.PoolableConnectionFactory;
+import org.apache.commons.dbcp2.PoolingDataSource;
+import org.apache.commons.pool2.impl.GenericObjectPool;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+@WebListener
+public class ContextListener implements ServletContextListener {
+    static final String JDBC_DRIVER = "org.postgresql.Driver";
+    static final String JDBC_DB_URL = "jdbc:postgresql://localhost:5432/bank";
+    static final String JDBC_USER = "postgres";
+    static final String JDBC_PASS = "qwqwqw";
+    private DataSource dataSource=null;
+    private GenericObjectPool gPool = null;
+    @Override
+    public void contextInitialized(ServletContextEvent servletContextEvent)  {
+        System.out.println("CONTEXT INIT");
+        final ServletContext servletContext = servletContextEvent.getServletContext();
+        try {
+            Class.forName(JDBC_DRIVER);
+            ConnectionFactory cf = new DriverManagerConnectionFactory(JDBC_DB_URL, JDBC_USER, JDBC_PASS);
+            PoolableConnectionFactory pcf = new PoolableConnectionFactory(cf, null);
+            gPool = new GenericObjectPool(pcf);
+            gPool.setMaxIdle(5);
+            dataSource = new PoolingDataSource(gPool);
+            servletContext.setAttribute("ds",dataSource);
+
+        } catch (Exception e){
+            System.out.println("SQL exeption occured");}
+
+
+        };
+
+
+    @Override
+    public void contextDestroyed(ServletContextEvent servletContextEvent) {
+
+    }
+}
